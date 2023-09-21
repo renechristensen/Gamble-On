12a135007205c5ddeafa3d650aae81790f58ed03
Rene Christensen <rene723@gmail.com>
2023-09-21 19:52:29 +0200

    Added AuthorizationService

---
+++ Gamble-On/Services/AuthorizationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamble_On.Services
+{
+    public class AuthorizationService : IAuthorizationService
+    {
+        public async Task SetAuthorizationHeader(HttpClient httpClient)
+        {
+            var token = await SecureStorage.GetAsync("auth_token");
+            if (string.IsNullOrEmpty(token))
+            {
+                throw new Exception("Token is missing or invalid");
+            }
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+}
---
+++ Gamble-On/Services/IAuthorizationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamble_On.Services
+{
+    public interface IAuthorizationService
+    {
+        Task SetAuthorizationHeader(HttpClient httpClient);
+    }
+}
